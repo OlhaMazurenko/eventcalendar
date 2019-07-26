c5ea7fef15da9843416413d0de89de615bcb2e5b
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-07-26 12:57:11 +0600

    Updates

---
+++ precache-manifest.cfaf10e815b5f140ca72a174c362c37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94fca8df8d2742c93cce3a5e8b4005c",
+    "url": "/eventcalendar/index.html"
+  },
+  {
+    "revision": "541847d194532f739954",
+    "url": "/eventcalendar/static/css/2.dcab8346.chunk.css"
+  },
+  {
+    "revision": "a8b9daf6b5f2f0e595b4",
+    "url": "/eventcalendar/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "541847d194532f739954",
+    "url": "/eventcalendar/static/js/2.42336b8b.chunk.js"
+  },
+  {
+    "revision": "a8b9daf6b5f2f0e595b4",
+    "url": "/eventcalendar/static/js/main.592bcfd3.chunk.js"
+  },
+  {
+    "revision": "5b246694ecab12f959af",
+    "url": "/eventcalendar/static/js/runtime~main.77ec7bbb.js"
+  }
+]);
